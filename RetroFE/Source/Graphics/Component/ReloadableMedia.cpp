dde9afa190218e4aa456541b630f2a6a763e63e7
inigomonyota <96964704+inigomonyota@users.noreply.github.com>
2024-07-12 20:08:36 -0400

    tie logos to selecteditem name

--- RetroFE/Source/Graphics/Component/ReloadableMedia.cpp
+++ RetroFE/Source/Graphics/Component/ReloadableMedia.cpp
@@ -206,6 +206,9 @@ Component* ReloadableMedia::reloadTexture()
     else if (isVideo_) {
         basename = selectedItem->name;
     }
+    else if (typeLC == "logo") {
+        basename = selectedItem->name;
+    }
     else {
         basename = "default";
     }
@@ -314,6 +317,9 @@ Component* ReloadableMedia::reloadTexture()
                 else {
                     name = std::to_string(static_cast<int>(ceil(static_cast<float>(position) / static_cast<float>(page.getCollectionSize()) * static_cast<float>(numberOfImages_))));
                 }
+            }
+            else if (typeLC == "logo") {
+                basename = selectedItem->name;
             }
 
             if (!selectedItem->leaf) {
