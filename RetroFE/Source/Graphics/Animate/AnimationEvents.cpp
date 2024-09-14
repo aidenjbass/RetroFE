9e4038277ce89c70cf7d4bbd3f47e866e9638df8
inigomonyota <96964704+inigomonyota@users.noreply.github.com>
2024-09-14 08:59:36 -0400

    Revert "small AnimationEvents optimization"
    This reverts commit 97388e05a21cc40ec49dd7d02a6f54fda49a3f1b.

--- RetroFE/Source/Graphics/Animate/AnimationEvents.cpp
+++ RetroFE/Source/Graphics/Animate/AnimationEvents.cpp
@@ -33,20 +33,14 @@ std::shared_ptr<Animation> AnimationEvents::getAnimation(const std::string& twee
 
 std::shared_ptr<Animation> AnimationEvents::getAnimation(const std::string& tween, int index)
 {
-    // Find the tween map, or insert a new map with the default -1 entry if not found
-    auto& indexMap = animationMap_[tween];
-    if (indexMap.find(-1) == indexMap.end()) {
-        indexMap[-1] = std::make_shared<Animation>();
-    }
+    if (animationMap_.find(tween) == animationMap_.end())
+        animationMap_[tween][-1] = std::make_shared<Animation>();
 
-    // Find the specific index, or fallback to -1 if the index is not found
-    if (indexMap.find(index) == indexMap.end()) {
+    if (animationMap_[tween].find(index) == animationMap_[tween].end())
         index = -1;
-    }
 
-    return indexMap[index];
+    return animationMap_[tween][index];
 }
-
 
 void AnimationEvents::setAnimation(const std::string& tween, int index, std::shared_ptr<Animation> animation)
 {
